e203c651c08765fd0d8062aa1b3babb3ebe4aadf
insane <luojieyy@gmail.com>
2019-08-19 17:44:50 +0800

    Initial commit

---
+++ .umirc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // plugins: ["umi-plugin-remax"],
+    // chainWebpack(config, { webpack }) {
+    //     // 设置 alias
+    //     config.resolve.alias.set('remax/wechat', 'remax/h5');
+    // },
+
+    alias: {
+        "remax/wechat": "remax/h5"
+    }
+}
