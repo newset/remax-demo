dd1551a08f6d6050b8c416ad2dd5e303d661823a
insane <luojieyy@gmail.com>
2019-09-09 11:26:03 +0800

    feat: use @remax/dva

--- src/app.js
+++ src/app.js
@@ -2,14 +2,13 @@
   @todo 接入 DVA
 */
 import React, { Component } from 'react';
-import dva from './dva';
-import { Provider } from 'react-redux'; // 引入后立即无法执行编译
+import dvaApp from './dva';
 import './app.css';
 
 class App extends Component {
   render() {
-    return <Provider store={dva._store}>{this.props.children}</Provider>;
+    return this.props.children;
   }
 }
 
-export default App;
+export default dvaApp.start(App);
